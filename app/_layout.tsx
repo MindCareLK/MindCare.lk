1228e0044bf28b93d9c96f9f57f9196cd9a931c1
Nirosha <niroshamadumali37@gmail.com>
2026-04-03 17:51:28 +0530

    added articles

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout() {
         <Stack.Screen name="login" options={{ headerShown: false }} />
         <Stack.Screen name="member-login" options={{ headerShown: false }} />
         <Stack.Screen name="counselor-login" options={{ headerShown: false }} />
+        <Stack.Screen name="articles" options={{ headerShown: false }} />
         <Stack.Screen
           name="counselor-register"
           options={{ headerShown: false }}
